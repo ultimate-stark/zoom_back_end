744a15816e0ed4c27e1a983a029290323c81120c
Ahmed Yasser <ahmedreo4@gmail.com>
2020-07-01 20:02:33 +0200

    fix

--- controllers/files.controller.js
+++ controllers/files.controller.js
@@ -1,5 +1,5 @@
 const Book = require("../models/book.model");
-var AdmZip = require('adm-zip');
+// var AdmZip = require('adm-zip');
 // var unzipper = require('unzipper');
 var XLSX = require('xlsx');
 var path = require('path');
@@ -43,31 +43,33 @@ exports.createBooks = (req, res, next) => {
           books[i].category.push({ title: col.category })
           books[i].subCategory.push({ title: col.subCategory })
           if (col.fileName) {
-            books[i].file.push({ link: col.file, name: col.fileName.split('.')[0] + '.' + 'pdf' })
+            books[i].file.push({ link: col.file, name: col.fileName })
             books[i].file.push({ link: 'https://zoom-books.s3.us-west-1.wasabisys.com/' + col.fileName })
           }
 
           if (col.file) {
             request({ url: col.file, encoding: null }, function (err, res, contents) {
-              var fileName;
               fs.writeFile(path.join(__dirname, `../images/${col.fileName}`), contents, function () {
 
-                var zip = new AdmZip(path.join(__dirname, `../images/${col.fileName}`));
-                var zipEntries = zip.getEntries();
+                // var zip = new AdmZip(path.join(__dirname, `../images/${col.fileName}`));
+                
+                // var zipEntries = zip.getEntries();
+                
+                // zipEntries.forEach(function (zipEntry) {
+                
+                //   fileName = zipEntry.entryName;
+                
+                // });
+                
+                // var zip = new AdmZip(path.join(__dirname, `../images/${col.fileName}`));
+                
+                // zip.extractAllTo(path.join(__dirname, `../images`), true);
+                
+                // var extension = fileName.split('.')[col.fileName.split('.').length - 1]
+                
+                // var newFileName = col.fileName.split('.')[0] + '.' + extension
 
-                zipEntries.forEach(function (zipEntry) {
-                  fileName = zipEntry.entryName;
-                });
-
-                var zip = new AdmZip(path.join(__dirname, `../images/${col.fileName}`));
-
-                zip.extractAllTo(path.join(__dirname, `../images`), true);
-
-                var extension = fileName.split('.')[col.fileName.split('.').length - 1]
-
-                var newFileName = col.fileName.split('.')[0] + '.' + extension
-
-                fs.renameSync(path.join(__dirname, `../images/${fileName}`), path.join(__dirname, `../images/${newFileName}`))
+                // fs.renameSync(path.join(__dirname, `../images/${fileName}`), path.join(__dirname, `../images/${newFileName}`))
 
                 s3.putObject({
                   Body: contents,
